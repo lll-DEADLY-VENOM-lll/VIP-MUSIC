841a2cd30daf6dc9d1398e4eff796469bfb9db00
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-27 16:16:25 +0530

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -64,14 +64,7 @@ async def ban_new(client, message):
 async def start_comm(client, message: Message, _):
     chat_id = message.chat.id
     await add_served_user(message.from_user.id)
-    
-    # --- Reaction Fix Start ---
-    try:
-        await message.react(reactions=["🕊️"])
-    except:
-        pass
-    # --- Reaction Fix End ---
-
+    await message.react("🕊️")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
