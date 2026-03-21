d818247670c0249ea55a2c41c00d9629f197a433
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-03-21 20:16:24 +0530

    0.3 version

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -1,31 +1,34 @@
 import json
 import os
 
-from VIPMUSIC.core.bot import VIPBot
+# FIRST: Import logging to make sure 'logger' and 'LOGGER' are available immediately
+from .logging import logger, LOGGER
+
+# SECOND: Setup directories and environment
 from VIPMUSIC.core.dir import dirr
 from VIPMUSIC.core.git import git
-from VIPMUSIC.core.userbot import Userbot
-from VIPMUSIC.core.youtube import vipboy
 from VIPMUSIC.misc import dbb, heroku, sudo
 
-from .logging import LOGGER
-
 dirr()
-
 git()
-
 dbb()
-
 heroku()
-
 sudo()
+
+# THIRD: Import core components
+# Note: These modules must NOT import "logger" from "VIPMUSIC" directly.
+# They should import from "VIPMUSIC.logging"
+from VIPMUSIC.core.bot import VIPBot
+from VIPMUSIC.core.userbot import Userbot
+from VIPMUSIC.core.youtube import vipboy
 
 vipboy()
 
+# FOURTH: Initialize the Bot Clients
 app = VIPBot()
-
 userbot = Userbot()
 
+# FIFTH: Initialize Platforms
 from .platforms import *
 
 YouTube = YouTubeAPI()
@@ -35,4 +38,5 @@ Apple = AppleAPI()
 Resso = RessoAPI()
 SoundCloud = SoundAPI()
 Telegram = TeleAPI()
+
 HELPABLE = {}
