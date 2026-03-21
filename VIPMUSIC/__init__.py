4bd9678d4d71583988607c86d8a6ec2073146276
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-03-21 20:24:31 +0530

    0.3 version

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -1,43 +1,31 @@
-import asyncio
 import json
 import os
-import uvloop
 
-# --- सबसे पहले Event Loop को सेटअप करें ---
-uvloop.install()
-try:
-    loop = asyncio.get_event_loop()
-except RuntimeError:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-# ---------------------------------------
-
-# अब बाकी के Imports करें
-from .logging import logger, LOGGER
-
+from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.dir import dirr
 from VIPMUSIC.core.git import git
-from VIPMUSIC.misc import dbb, heroku, sudo
-
-# Directories और Setup
-dirr()
-git()
-dbb()
-heroku()
-sudo()
-
-# Core Components को Import करें
-from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.userbot import Userbot
 from VIPMUSIC.core.youtube import vipboy
+from VIPMUSIC.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
 
 vipboy()
 
-# अब Bots को Initialize करें (अब एरर नहीं आएगा)
 app = VIPBot()
+
 userbot = Userbot()
 
-# Platforms Setup
 from .platforms import *
 
 YouTube = YouTubeAPI()
@@ -47,5 +35,4 @@ Apple = AppleAPI()
 Resso = RessoAPI()
 SoundCloud = SoundAPI()
 Telegram = TeleAPI()
-
 HELPABLE = {}
