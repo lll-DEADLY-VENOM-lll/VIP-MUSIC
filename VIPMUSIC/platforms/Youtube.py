fefbdfa38df60d1eab70bac8d0d192afa2dd85b1
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-04-20 19:31:43 +0530

    0.3 version

--- VIPMUSIC/platforms/Youtube.py
+++ VIPMUSIC/platforms/Youtube.py
@@ -32,7 +32,7 @@ class SensitiveDataFilter(logging.Filter):
 
 logging.getLogger().addFilter(SensitiveDataFilter())
 
-API_URL = "http://kiru-bot.up.railway.app"
+API_URL = "https://kiru-bot.up.railway.app"
 
 # --- UTILS ---
 def get_clean_id(link: str) -> Optional[str]:
